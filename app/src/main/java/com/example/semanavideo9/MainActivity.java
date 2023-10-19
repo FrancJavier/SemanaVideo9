118b3a12fee873fbfe5d349899586a7278c2bcc1
Francisco <francisco.javier2507@gmail.com>
2023-10-19 17:26:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/semanavideo9/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.semanavideo9;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.semanavideo9.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView mivideo = findViewById(R.id.viVisor);
+        String videop = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videop);
+        mivideo.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mivideo.setMediaController(mediaController);
+        mediaController.setAnchorView(mivideo);
+
+    }
+}
